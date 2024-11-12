435a9039baea40fa9088d3e7e5d72822adf925d3
k3ym <artemurusov2016@gmail.com>
2024-11-12 12:50:01 +0300

    help-window

--- script.js
+++ script.js
@@ -17,13 +17,31 @@ infoBtn = document.querySelector('.info-btn');
 
 mainBtn = document.querySelector('.mainRedirect');
 
+recsBtn = document.querySelector('.header-other');
+
 let page_redirect = function(object2show, object2hide){
     object2show.style.display = 'flex';
     object2hide.style.display = 'none';
-}
+};
 
-indexPage = document.querySelector('.index')
-recPage = document.querySelector('.records')
+indexPage = document.querySelector('.index');
+recPage = document.querySelector('.records');
 
 infoBtn.addEventListener('click', () => page_redirect(recPage, indexPage));
+recsBtn.addEventListener('click', () => page_redirect(recPage, indexPage));
 mainBtn.addEventListener('click', () => page_redirect(indexPage, recPage));
+
+/* help section */
+
+let help = document.querySelector('.help');
+let close = document.querySelector('.help-close')
+
+help.addEventListener('click', function(){
+    let helpWindow = document.querySelector('.help-window');
+    helpWindow.style.display = 'flex';
+});
+
+close.addEventListener('click', function(){
+    let helpWindow = document.querySelector('.help-window');
+    helpWindow.style.display = 'none';
+})
